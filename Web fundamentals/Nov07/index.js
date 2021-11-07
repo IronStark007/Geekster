0df7ebb027c2d1fb1db3e4e6b6e8815baa031293
IronStark007 <ansar326629@gmail.com>
2021-11-07 16:59:27 +0530

    Countdown timer done

---
+++ Web fundamentals/Nov07/index.js
@@ -0,0 +1,23 @@
+var deadline = new Date("nov 31, 2021 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.getElementById("day").innerHTML = days;
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("min").innerHTML = minutes;
+    document.getElementById("sec").innerHTML = seconds;
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("over").innerHTML = 'TIMES UP';
+        document.getElementById("day").innerHTML = '0';
+        document.getElementById("hour").innerHTML = '0';
+        document.getElementById("min").innerHTML = '0';
+        document.getElementById("sec").innerHTML = '0';
+    }
+}, 1000);
