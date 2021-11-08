f268b200244e84788bc1234fe77224f3f12d920e
IronStark007 <ansar326629@gmail.com>
2021-11-08 19:56:37 +0530

    Countdown timer updated

--- Web fundamentals/Nov07/index.js
+++ Web fundamentals/Nov07/index.js
@@ -1,23 +1,23 @@
-var deadline = new Date("nov 31, 2021 15:37:25").getTime();
+var deadline = new Date("oct 31, 2021 15:37:25").getTime();
 
-var x = setInterval(function() {
+var time_interval = setInterval(function() {
 
     var now = new Date().getTime();
-    var t = deadline - now;
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    var future_time = deadline - now;
+    var days = Math.floor(future_time / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((future_time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((future_time % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((future_time % (1000 * 60)) / 1000);
     document.getElementById("day").innerHTML = days;
     document.getElementById("hour").innerHTML = hours;
     document.getElementById("min").innerHTML = minutes;
     document.getElementById("sec").innerHTML = seconds;
-    if (t < 0) {
-        clearInterval(x);
-        document.getElementById("over").innerHTML = 'TIMES UP';
-        document.getElementById("day").innerHTML = '0';
-        document.getElementById("hour").innerHTML = '0';
-        document.getElementById("min").innerHTML = '0';
-        document.getElementById("sec").innerHTML = '0';
+    if (future_time < 0) {
+        clearInterval(time_interval);
+        document.getElementById("over").innerHTML = 'TIMES UP!!!';
+        document.getElementById("day").innerHTML = '00';
+        document.getElementById("hour").innerHTML = '00';
+        document.getElementById("min").innerHTML = '00';
+        document.getElementById("sec").innerHTML = '00';
     }
 }, 1000);
