ce6699a4aa411c7bdda184638df7efd859fe8b29
Mohd Ali Ansari <ansar326629@gmail.com>
2022-06-26 17:54:24 +0530

    Chnage authen method

--- Advanced Frontend/Week-6/Sunday-test/articles/src/util.js
+++ Advanced Frontend/Week-6/Sunday-test/articles/src/util.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const newsApi = axios.create({
     baseURL: 'https://newsapi.org/v2/',
     timeout: 10000,
-    headers: {'X-Api-Key': process.env.REACT_APP_API_KEY}
+    headers: {'Authorization': process.env.REACT_APP_API_KEY}
   });
